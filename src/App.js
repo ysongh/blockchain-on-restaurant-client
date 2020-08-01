082faf262e80f67596f695157db00b18cdae58d7
You Song Hou <yousonghou@yahoo.com>
2020-07-31 22:03:38 -0400

    add list of restaurants page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import './App.css';
 import Navbar from './component/layout/Navbar';
 import Home from './component/Home';
-import AddRestaurant from './component/AddRestaurant';
+import Restaurants from './component/restaurant/Restaurants';
+import AddRestaurant from './component/restaurant/AddRestaurant';
 
 function App() {
   return (
     <Router className="App">
       <Navbar />
       <Route exact path="/" component={Home} />
-      <Route exact path="/restaurant" component={AddRestaurant} />
+      <Route exact path="/restaurant" component={Restaurants} />
+      <Route exact path="/addrestaurant" component={AddRestaurant} />
     </Router>
   );
 }
---
+++ src/component/restaurant/Restaurants.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Restaurant = () => {
+    const [data, setData] = useState([]);
+    const [go] = useState(true);
+
+    useEffect(() => {
+        async function getRestaurants() {
+            try{
+                const { data } = await axios.get('http://localhost:1000/api/restaurant');
+    
+                setData(data.data);
+            } catch(err){
+                console.error(err);
+            }
+        }
+        
+        getRestaurants();
+    }, [go]);
+
+    return(
+        <div className="container">
+            <h1 className="text-center">Restaurant</h1>
+            <div className="row">
+                { data.length ? (
+                    data.map(restaurant => {
+                        return(
+                            <div className="col-12 col-md-6 col-lg-4" key={restaurant._id}>
+                                <div className="card">
+                                    <img className="card-img-top" src={restaurant.image} alt="Image of the restaurant" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{restaurant.name}</h5>
+                                        <p className="card-text">{restaurant.location}</p>
+                                        <Link to="/" className="btn btn-primary">See Deals</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                ) : <p>No Restaurant</p>}
+            </div>
+        </div>
+    );
+};
+
+export default Restaurant;
