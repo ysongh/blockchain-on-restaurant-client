6412f84874461b16e09867ddc5c1e408c7521970
You Song Hou <yousonghou@yahoo.com>
2020-08-03 23:49:39 -0400

    add a link to production server

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 let serverURL = 'http://localhost:1000/api/';
 
+if(process.env.NODE_ENV === 'production'){
+    serverURL = 'https://eatoutcoinserver.herokuapp.com/api/';
+}
+
 const instance = axios.create({
    baseURL: serverURL
 });
