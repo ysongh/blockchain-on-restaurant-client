4ecbc28bf685fb86764efd479d440122153bad90
You Song Hou <yousonghou@yahoo.com>
2020-08-05 00:01:56 -0400

    add icons

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import HeroImage from '../assets/hero.png';
+import Icon1 from '../assets/icon1.svg';
+import Icon2 from '../assets/icon2.svg';
+import Icon3 from '../assets/icon3.svg';
 
 const Home = () => {
     return(
@@ -21,6 +24,33 @@ const Home = () => {
                 </div>
                 <div className="col-12 col-md-6">
                     <img src={HeroImage} alt="Hero" />
+                </div>
+            </div>
+
+            <div className="row my-5">
+                <div className="col-12 col-md-6 col-lg-4 mt-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <img className="icon" src={Icon3} alt="Icon" />
+                            <h5 className="card-title text-center mt-3">Earn Coins</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4 mt-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <img className="icon" src={Icon1} alt="Icon" />
+                            <h5 className="card-title text-center mt-3">Any Restaurant</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4 mt-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <img className="icon" src={Icon2} alt="Icon" />
+                            <h5 className="card-title text-center mt-3">Get Rewards</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
