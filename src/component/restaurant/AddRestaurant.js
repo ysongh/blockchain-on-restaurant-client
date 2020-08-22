e5a82418d5dbc68515f7328853dab91be99217a9
You Song Hou <yousonghou@yahoo.com>
2020-08-21 21:10:42 -0400

    add an image preview on the restaurant form page

--- src/component/restaurant/AddRestaurant.js
+++ src/component/restaurant/AddRestaurant.js
@@ -13,10 +13,11 @@ const AddRestaurant = () => {
     const [name, setName] = useState('');
     const [location, setLocation] = useState('');
     const [description, setDescription] = useState('');
-    const [image, setImage] = useState('');
+    const [imageFile, setImageFile] = useState('');
     const [imageName, setImageName] = useState('Choose File');
     const [loading, setLoading] = useState(false);
     const [go] = useState(true);
+    const [preview, setPreview] = useState();
 
     useEffect(() => {
         async function getRestaurantInfo() {
@@ -35,11 +36,25 @@ const AddRestaurant = () => {
 
     }, [go, id]);
 
-    const selectFile = e => {
-        if(e.target.files[0]){
-            setImage(e.target.files[0]);
-            setImageName(e.target.files[0].name);
+    useEffect(() => {
+        if(!imageFile){
+            setPreview(undefined);
+            return;
         }
+
+        const objectUrl = URL.createObjectURL(imageFile);
+        setPreview(objectUrl);
+
+        return () => URL.revokeObjectURL(objectUrl);
+    }, [imageFile]);
+
+    const selectFile = e => {
+        if(!e.target.files || e.target.files.length === 0){
+            setImageFile(undefined);
+            return;
+        }
+        setImageFile(e.target.files[0]);
+        setImageName(e.target.files[0].name);
     }
 
     const onSubmit = async () => {
@@ -50,7 +65,7 @@ const AddRestaurant = () => {
             formData.append('name', name);
             formData.append('location', location);
             formData.append('description', description);
-            formData.append('image', image);
+            formData.append('image', imageFile);
 
             if(id){
                 await axios.put('/restaurant/' + id, formData);
@@ -101,6 +116,7 @@ const AddRestaurant = () => {
                                 <label className="custom-file-label" htmlFor="inputGroupFile01">{imageName}</label>
                             </div>
                         </div>
+                        {imageFile && <img src={preview} alt="Preview" /> }
                     </div>
                 </div>
                 
