f83c78ae3e5f8fb5c56028d489678422959d5c24
You Song Hou <yousonghou@yahoo.com>
2020-11-02 23:39:40 -0500

    add links on the footer

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return(
-        <footer className="primary-color p-4 text-center">
-            Copyright &copy;{new Date().getFullYear()} Eat Out Coin
+        <footer className="primary-color py-4 ">
+            <div className="d-flex justify-content-center mb-2">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/deal">Deals</Link>
+                <Link className="nav-link" to="/restaurant">Restaurant</Link>
+                <Link className="nav-link" to="/coin">Coin</Link>
+                <Link className="nav-link" to="/register">Get Started</Link>
+            </div>
+            <p className="text-center">Copyright &copy;{new Date().getFullYear()} Eat Out Coin</p>
         </footer>
     );
 };
