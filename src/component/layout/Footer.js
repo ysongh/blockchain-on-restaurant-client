2e4a1f4d372187a159a76cf57e86259e605ed60a
You Song Hou <yousonghou@yahoo.com>
2020-11-03 23:48:08 -0500

    add a logo on the footer

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../../assets/logo2.png';
+
 const Footer = () => {
     return(
-        <footer className="primary-color py-4 ">
-            <div className="d-flex justify-content-center mb-2">
+        <footer className="primary-color py-4">
+            <img className="logo d-block m-auto" src={Logo} alt="Logo" />
+            <div className="d-flex justify-content-center mt-1 mb-2">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/deal">Deals</Link>
                 <Link className="nav-link" to="/restaurant">Restaurant</Link>
