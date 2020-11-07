7debf0bcc1c2547e0ff34cc71e577a936f170e8a
You Song Hou <yousonghou@yahoo.com>
2020-11-07 00:35:48 -0500

    display a list of restaurants created by the owner

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import axios from '../axios';
+import Moment from 'react-moment';
 
 import { GlobalContext } from '../context/GlobalState';
+import DefaultImage from '../assets/noimage.png';
 
 const Profile = () => {
     const { ownerId } = useContext(GlobalContext);
@@ -25,6 +28,31 @@ const Profile = () => {
     return(
         <div className="container">
             <h1>{data.name}</h1>
+            <div className="row">
+                {
+                    data.restaurants.map(restaurant => {
+                        return(
+                            <div className="col-12 col-md-6 col-lg-4" key={restaurant._id}>
+                                <div className="card mb-3">
+                                    <Link to={`/restaurant/${restaurant._id}`}>
+                                        <img className="card-img-height card-img-top" src={restaurant.image || DefaultImage} alt="Restaurant" />
+                                    </Link>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{restaurant.name}</h5>
+                                        <p className="card-text">{restaurant.location}</p>
+                                        <Link to={`/restaurant/${restaurant._id}`} className="btn primary-color">See Deals</Link>
+                                        <p className="card-text">
+                                            <small className="text-muted">
+                                                Post On <Moment format="MM/DD/YYYY">{restaurant.date}</Moment>
+                                            </small>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
