e070255e83b186014fcd8fc555ba68533a432551
You Song Hou <yousonghou@yahoo.com>
2020-11-20 22:54:08 -0500

    display deals on the profile page

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -14,13 +14,22 @@ const Profile = () => {
     useEffect(() => {
         async function getProfile() {
             try{
-                const { data } = await axios.get('/owner/profile/' + ownerId);
+                const restaurantsData = await axios.get('/owner/profile/' + ownerId);
                 
-                console.log(data.data)
-                setData(data.data);
+                for(let restaurant of restaurantsData.data.data.restaurants){
+                    const deals = await getDeals(restaurant._id);
+                    restaurant["deals"] = deals;
+                }
+
+                setData(restaurantsData.data.data);
             } catch(err){
                 console.error(err);
             }
+        }
+
+        async function getDeals(restaurantId){
+            const {data} = await axios.get('/deal/' + restaurantId);
+            return data.data;
         }
         
         getProfile();
@@ -34,32 +43,41 @@ const Profile = () => {
                     Add Restaurant
                 </Link>
             </div>
-            
-            <div className="row">
-                {
-                    data.restaurants.map(restaurant => {
-                        return(
-                            <div className="col-12 col-md-6 col-lg-4" key={restaurant._id}>
-                                <div className="card mb-3">
-                                    <Link to={`/restaurant/${restaurant._id}`}>
-                                        <img className="card-img-height card-img-top" src={restaurant.image || DefaultImage} alt="Restaurant" />
-                                    </Link>
-                                    <div className="card-body">
-                                        <h5 className="card-title">{restaurant.name}</h5>
-                                        <p className="card-text">{restaurant.location}</p>
-                                        <Link to={`/restaurant/${restaurant._id}`} className="btn primary-color">See Deals</Link>
-                                        <p className="card-text">
-                                            <small className="text-muted">
-                                                Post On <Moment format="MM/DD/YYYY">{restaurant.date}</Moment>
-                                            </small>
-                                        </p>
-                                    </div>
+
+            { data.restaurants.map(restaurant => {
+                return(
+                    <div className="row" key={restaurant._id}>
+                        <div className="col-12 col-md-6 col-lg-4" >
+                            <div className="card mb-3">
+                                <Link to={`/restaurant/${restaurant._id}`}>
+                                    <img className="card-img-height card-img-top" src={restaurant.image || DefaultImage} alt="Restaurant" />
+                                </Link>
+                                <div className="card-body">
+                                    <h5 className="card-title">{restaurant.name}</h5>
+                                    <p className="card-text">{restaurant.location}</p>
+                                    <Link to={`/restaurant/${restaurant._id}`} className="btn primary-color">See Deals</Link>
+                                    <p className="card-text">
+                                        <small className="text-muted">
+                                            Post On <Moment format="MM/DD/YYYY">{restaurant.date}</Moment>
+                                        </small>
+                                    </p>
                                 </div>
                             </div>
-                        )
-                    })
-                }
-            </div>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-8">
+                            { restaurant.deals && restaurant.deals.map(deal => {
+                                return(
+                                    <div key={deal._id}>
+                                        <h2>{deal.name}</h2>
+                                        <p>{deal.price}</p>
+                                        <p>{deal.description}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     );
 };
