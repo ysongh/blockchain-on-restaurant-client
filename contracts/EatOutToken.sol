4cae2289a12eb78a2b6ed0fd8cd68ba2cbb39873
You Song Hou <yousonghou@yahoo.com>
2020-12-05 21:28:03 -0500

    save deployer's address to contract

--- contracts/EatOutToken.sol
+++ contracts/EatOutToken.sol
@@ -6,10 +6,12 @@ contract EatOutToken is ERC20Mintable{
     string  public name;
     string  public symbol;
     uint256 public decimals;
+    address payable admin;
 
     constructor() public {
         name = "Eat Out Coin";
         symbol = "EOTC";
         decimals = 18;
+        admin = msg.sender;
     }
 }
