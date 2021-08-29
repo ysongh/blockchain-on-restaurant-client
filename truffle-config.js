e96edfd9038ca9bd1d4764ac266c7ecde3af0a00
You Song Hou <yousonghou@gmail.com>
2021-08-29 01:27:26 -0400

    update truffle-config to deploy contracts on Rinkeby Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.MNEMONIC.toString().trim();
 
 module.exports = {
   networks: {
@@ -5,7 +8,15 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
-    }
+    },
+    // truffle migrate --network rinkeby
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
+      network_id: 4,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
