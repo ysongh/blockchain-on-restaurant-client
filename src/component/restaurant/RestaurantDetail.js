67f5b7f0aec509d711f73a1bc0155fa7ecceaf9d
You Song Hou <yousonghou@gmail.com>
2021-09-01 00:49:10 -0400

    add a function to called contract supply()

--- src/component/restaurant/RestaurantDetail.js
+++ src/component/restaurant/RestaurantDetail.js
@@ -4,11 +4,15 @@ import Moment from 'react-moment';
 
 import axios from '../../axios';
 import DefaultImage from '../../assets/noimage.png';
+import Spinner from '../common/Spinner';
+import { GlobalContext } from '../../context/GlobalState';
 
 const RestaurantDetail = () => {
     const { id } =  useParams();
+    const { account, contract } = useContext(GlobalContext);
 
     const [data, setData] = useState({deals: []});
+    const [loading, setLoading] = useState(false);
     const [go] = useState(true);
 
     useEffect(() => {
@@ -24,6 +28,20 @@ const RestaurantDetail = () => {
         
         getRestaurants();
     }, [go, id]);
+
+    const buyFood = async price => {
+        try{
+            setLoading(true);
+            const res = await contract.methods.supply().send({ from: account, value: window.web3.utils.toWei(price.toString(), 'Ether') });
+            console.log(res);
+            setLoading(false);
+        }
+        catch(err){
+            console.error(err);
+            setLoading(false);
+        }
+        
+    }
 
     return(
         <div className="container">
@@ -47,29 +65,38 @@ const RestaurantDetail = () => {
                 </div>
             </div>
             <hr />
-            <div className="row">
-                { data.deals.length ? (
-                    data.deals.map(deal => {
-                        return(
-                            <div className="col-12 col-md-6 col-lg-4" key={deal._id}>
-                                <div className="card mb-3">
-                                    <img className="card-img-top card-img-height" src={deal.image || DefaultImage} alt="Deal" />
-                                    <span className="badge  badge-pill badge-info badge-price">ETH {deal.price}</span>
-                                    <div className="card-body">
-                                        <h5 className="card-title">{deal.name}</h5>
-                                        <p className="card-text">{deal.description}</p>
-                                        <p className="card-text">
-                                            <small className="text-muted">
-                                                Post On <Moment format="MM/DD/YYYY">{deal.date}</Moment>
-                                            </small>
-                                        </p>
+            {loading
+                ? <Spinner />
+                : <div className="row">
+                    { data.deals.length ? (
+                        data.deals.map(deal => {
+                            return(
+                                <div className="col-12 col-md-6 col-lg-4" key={deal._id}>
+                                    <div className="card mb-3">
+                                        <img className="card-img-top card-img-height" src={deal.image || DefaultImage} alt="Deal" />
+                                        <span className="badge  badge-pill badge-info badge-price">ETH {deal.price}</span>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{deal.name}</h5>
+                                            <p className="card-text">{deal.description}</p>
+                                            <p className="card-text">
+                                                <small className="text-muted">
+                                                    Post On <Moment format="MM/DD/YYYY">{deal.date}</Moment>
+                                                </small>
+                                            </p>
+                                            <button
+                                                className="btn secondary-color btn-lg"
+                                                onClick={() => buyFood(deal.price)}
+                                                disabled={!contract}>
+                                                {contract ? "Buy" : "Collect to Wallet"}
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                ) : <p className="mt-5 h4 text-danger">No Deals</p>}
-            </div>
+                            )
+                        })
+                    ) : <p className="mt-5 h4 text-danger">No Deals</p>}
+                </div>
+            }
         </div>
     );
 };
